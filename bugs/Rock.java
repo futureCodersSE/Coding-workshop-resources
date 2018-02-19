ccb3e597236f93186abd8ff827df99df7730368a
Karsco <karsco73@gmail.com>
2018-02-19 11:43:29 +0000

    Empty scenario for students

---
+++ bugs/Rock.java
@@ -0,0 +1,16 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Rock here.
+ * 
+ * @author (futureCodersSE) 
+ * @version (v1 template 5th February 2018)
+ */
+public class Rock extends Actor
+{
+    //Act - rock has no actions
+    public void act() 
+    {
+        
+    }
+}
